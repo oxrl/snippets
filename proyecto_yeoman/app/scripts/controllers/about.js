03ab01a3cbac051f842ec8aa5ae1f9502e5b6ca1
Xavier Romero <o.romero.lopez@gmail.com>
2016-05-07 00:28:09 -0700

    Se anadio el Proyecto Yeoman

---
+++ proyecto_yeoman/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proyectoYeomanApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the proyectoYeomanApp
+ */
+angular.module('proyectoYeomanApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ proyecto_yeoman/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proyectoYeomanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the proyectoYeomanApp
+ */
+angular.module('proyectoYeomanApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
